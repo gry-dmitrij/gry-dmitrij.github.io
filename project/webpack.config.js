616a44b71fb4dcd0e046abea5c5bb6389732989a
Dmitriy Gryaznykh <gry-dmitrij@yandex.ru>
2021-08-19 18:48:14 +0300

    - В настройки добавлены загрузки шрифтов, картинок, а также лоэдер sass-bulk

--- project/webpack.config.js
+++ project/webpack.config.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const HtmlPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+const { extendDefaultPlugins } = require('svgo');
 //const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 const inputDir = 'src';      // входная директория
@@ -37,7 +39,7 @@ module.exports = (env, options) => {
 					test: /\.m?js$/,
 					exclude: /node_modules/,
 					use: {
-						loader: "babel-loader",
+						loader: 'babel-loader',
 						options: {
 							presets: ['@babel/preset-env']
 						}
@@ -54,23 +56,64 @@ module.exports = (env, options) => {
                                 }
                             },
                         'css-loader',
-                        'sass-loader'
+                        'sass-loader',
+						'sass-bulk-import-loader'
                     ]
                 },
                 {
-                    test: /\.(mp3|wav|aac)/i,
+                    test: /\.(mp3|wav|aac)$/i,
                     type: 'asset/resource',
                     generator: {
                         filename: 'sound/[hash][ext]'
                     }
-                }
+                },
+				{
+					test: /\.(woff2?|ttf)$/i,
+					type: 'asset/resource',
+					generator: {
+						filename: 'font/[name][ext]'
+					}
+				},
+				{
+					test: /img\/*.(jpe?g|png|gif|svg)$/i,
+					type: 'asset',
+					generator: {
+						filename: 'img/[hash][ext]'
+					}
+				}
             ]
         },
         plugins: [
             new MiniCssExtractPlugin({
-                filename: 'css/style.css',
+                filename: 'css/style.[hash].css',
 
             }),
+			new ImageMinimizerPlugin({
+				minimizerOptions: {
+					plugins: [
+						['gifsicle', { interlaced: true }],
+						['jpegtran', { progressive: true }],
+						['optipng', { optimizationLevel: 5 }],
+						[
+							'svgo',
+							{
+								plugins: extendDefaultPlugins([
+									{
+										name: 'removeViewBox',
+										active: false
+									},
+									{
+										name: 'addAttributesToSVGElement',
+										params: {
+											attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }],
+										}
+									}
+								])
+							}
+						]
+					]
+				}
+			}),
             new CleanWebpackPlugin(),
             //new BundleAnalyzerPlugin()
         ].concat(generateHTMLPlugins())
